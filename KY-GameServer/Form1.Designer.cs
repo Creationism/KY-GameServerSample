01d9667cdba39d2d9c364adf1f278d615aff8e97
Lorychenko <bubukirichenko@gmail.com>
2015-05-05 20:04:16 +0200

    Update 1
    Added most classes and server functionality.

--- KY-GameServer/Form1.Designer.cs
+++ KY-GameServer/Form1.Designer.cs
@@ -28,20 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.startServerBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ip = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.coords = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.area = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.classrole = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.ping = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -54,26 +55,99 @@
             this.dataGridView1.AllowUserToResizeColumns = false;
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.name,
             this.ip,
             this.coords,
             this.area,
-            this.classrole});
+            this.classrole,
+            this.ping});
             this.dataGridView1.Location = new System.Drawing.Point(6, 19);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(593, 277);
+            this.dataGridView1.Size = new System.Drawing.Size(638, 277);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(650, 305);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Players";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 323);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(650, 143);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Activity";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(638, 118);
+            this.textBox1.TabIndex = 1;
+            // 
+            // startServerBtn
+            // 
+            this.startServerBtn.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startServerBtn.Location = new System.Drawing.Point(92, 472);
+            this.startServerBtn.Name = "startServerBtn";
+            this.startServerBtn.Size = new System.Drawing.Size(118, 22);
+            this.startServerBtn.TabIndex = 4;
+            this.startServerBtn.Text = "Start server";
+            this.startServerBtn.UseVisualStyleBackColor = true;
+            this.startServerBtn.Click += new System.EventHandler(this.startServer);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(340, 472);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 22);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kick all players";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(216, 472);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 22);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Restart server";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(464, 472);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 22);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Clear activity log";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // name
             // 
@@ -100,88 +174,21 @@
             this.classrole.HeaderText = "Class";
             this.classrole.Name = "classrole";
             // 
-            // groupBox1
+            // ping
             // 
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(605, 305);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Players";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 323);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(605, 143);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Activity";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(6, 19);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(593, 118);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(70, 472);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 22);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Start server";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(318, 472);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 22);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Kick all players";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(194, 472);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 22);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Restart server";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(442, 472);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(118, 22);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Clear activity log";
-            this.button4.UseVisualStyleBackColor = true;
+            this.ping.HeaderText = "Ping";
+            this.ping.Name = "ping";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(630, 501);
+            this.ClientSize = new System.Drawing.Size(674, 501);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startServerBtn);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -197,18 +204,19 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button startServerBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn ip;
         private System.Windows.Forms.DataGridViewTextBoxColumn coords;
         private System.Windows.Forms.DataGridViewTextBoxColumn area;
         private System.Windows.Forms.DataGridViewTextBoxColumn classrole;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ping;
     }
 }
 
--- KY-GameServer/Form1.cs
+++ KY-GameServer/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Lidgren.Network;
+using System.Threading;
 
 namespace KY_GameServer
 {
@@ -21,14 +22,251 @@ namespace KY_GameServer
         //Network properties
         NetServer Server { get; set; }
         NetPeerConfiguration Config { get; set; }
-        DateTime time { get; set; }
-        TimeSpan timeToPass { get; set; }
+        DateTime time { get; set; } //In case we want to keep track of server time..?
         NetIncomingMessage inc { get; set; }
-        bool isServerRunning { get; set; }
+        bool serverIsRunning { get; set; }
 
-        private void button1_Click(object sender, EventArgs e)
+        //Actual game info data
+        List<Character> ConnectedPlayers { get; set; }
+        List<Zone> ActiveZones { get; set; }
+
+        private void startServer(object sender, EventArgs e)
+        {
+            //Create a new config instance, used to configure the server settings. Ky is the identifier for our network relationship in this case
+            Config = new NetPeerConfiguration("Ky");
+            //Listening on port 3000, clients must connect to port 3000 if they want to connect to us.
+            Config.Port = 3000;
+            //Enables it so users have to 'login'/'be approved' to join our network
+            Config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+            //Create new server instance with the Config we've created
+            Server = new NetServer(Config);
+            //[sass]I don't know what this does..[/sass]
+            Server.Start();
+
+            //Let it be known that the server has started, hallelujah
+            Write("Server started on port: " + Config.Port);
+
+            //Create our data instances to keep track of our game world
+            ConnectedPlayers = new List<Character>();
+
+            //We listen for clients on a separate Thread. This is a bit of an 'advanced' topic. You can ask me more about it if you're curious
+            //But the main reason for this one is so, it does not perform these actions on our 'Main' thread. This thread is used for our UI
+            //If we perform too many functions on our UI thread. Our UI may lag, so.. there's that!
+            Thread t = new Thread(new ThreadStart(listenForMessages));
+            t.Start();
+
+            serverIsRunning = true;
+        }
+
+        //In our listenForMessages we listen for all the messages we receive from clients. This is kind of the 'guts' of the server.
+        //Processing messages
+        private void listenForMessages()
+        {
+            while(serverIsRunning)
+            {
+                //If our message buffer isn't null, it means we have received a new message
+                if((inc = Server.ReadMessage()) != null) 
+                {
+                    //Switch statement to weed through the message types and handle each messagetype accordingly
+                    switch(inc.MessageType)
+                    {
+                        //These cases should be self explanatory. I'll add the following;
+                        //1. ConnectionApproval is if someone is trying to login
+                        //2. Data is for ANYTHING game related, movement, combat, chat, anything the user wants to tell us while he's logged into our game
+                        //3. Status changed if we want to capture a disconnected player and log it, can also be used for connect.. and more. 
+
+                        case NetIncomingMessageType.ConnectionApproval:
+                            byte loginbyte = inc.ReadByte();
+
+                            if(loginbyte == (byte)PacketTypes.LOGIN)
+                            {
+                                newLogin();
+                            }
+                            break;
+
+                        case NetIncomingMessageType.Data:
+                            byte dataByte = inc.ReadByte();
+
+                            if(dataByte == (byte)PacketTypes.MOVE)
+                            {
+                                updateMovements();
+                            }
+
+                            break;
+
+                        case NetIncomingMessageType.StatusChanged:
+
+                            //If a player is disconnected or is disconnecting.. remove him from our list
+                            if (inc.SenderConnection.Status == NetConnectionStatus.Disconnected || inc.SenderConnection.Status == NetConnectionStatus.Disconnecting)
+                            {
+                                removeDisconnectedUser(inc.SenderConnection);
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        //This function will manage all the movement and coordination across players (notifies other players if someone has moved, to reflect that and stuff) 
+        private void updateMovements()
+        {
+            
+        }
+
+
+        private void newLogin()
+        {
+            //When a player logs in, we expect that he sends his character info to us. 
+            //Normally he would only send his username and password but because we aren't using a database model
+            //We'll just create the users character in their own client. Maybe database example next time
+            //FYI: The connecting player can manually specify the zone he wants to connect to, easily changed by just setting the property though
+
+            //Create new instance of player then read all the properties the client has sent into this class
+            Character newlyConnectedPlayer = new Character();
+            inc.ReadAllProperties(newlyConnectedPlayer);
+
+            //If the player that is trying to connects' name already exists in our active player list
+            //We will refuse his connection. This can be handy, if you want to allow only one character (obviously) or one
+            //connection per IP Address, easy to modify
+            if(ConnectedPlayers.FirstOrDefault(f => f.Name == newlyConnectedPlayer.Name) != null)
+            {
+                inc.SenderConnection.Deny("You're already connected");
+                Write("Refused player with name " + newlyConnectedPlayer.Name + " because he was already connected.."); // LET IT BE KNOWN!
+                return;
+            }
+
+            //We give our player a connection property so we can keep connection info about him, like IP Address/ping, etc!
+            newlyConnectedPlayer.Connection = inc.SenderConnection;
+
+            //Let it be known that this player has connected to our wonderful game
+            Write(newlyConnectedPlayer.Name + " has connected!");
+
+            //Approve this fine gentleman into our secret society
+            inc.SenderConnection.Approve();
+
+            //Add this fine lad to the list of connected players
+            ConnectedPlayers.Add(newlyConnectedPlayer);
+
+
+            //Now it gets a little more interesting! 
+            //*~*~*~*~* (SPARKLES TO MAKE IT MORE MAGICAL!) *~*~*~*~*
+
+            //Worldstate (Can specify a name yourself) messages send the current zone state to players
+            
+            NetOutgoingMessage outmsg = Server.CreateMessage(); //We create a new message
+            outmsg.Write((byte)PacketTypes.WORLDSTATE); // Of type WORLDSTATE
+
+            //Get the amount of players the zone the new player is in
+            outmsg.Write(ConnectedPlayers.Where(f => f.CurrentZone == newlyConnectedPlayer.CurrentZone).Count()); //notice 'Count'
+
+            //For each player in this players' zone, send him the data of the players. (The players' client will process this info and draw them on his screen)
+            foreach (Character ch in ConnectedPlayers.Where(x => x.CurrentZone == newlyConnectedPlayer.CurrentZone))
+            {
+                outmsg.WriteAllProperties(ch);
+            }
+            Server.SendMessage(outmsg, inc.SenderConnection, NetDeliveryMethod.ReliableOrdered, 0); // Send the message, reliably and ordered
+
+            // LET IT BE KNOWN!
+            Write(String.Format("{0} : {1}  - has connected to the server and is in {2}", newlyConnectedPlayer.Name, 
+                                                                                          newlyConnectedPlayer.Connection.RemoteEndpoint.Address, 
+                                                                                          newlyConnectedPlayer.CurrentZone));
+
+            //Update our UI
+            updateList();
+        }
+
+        //Function to display users in our list overview
+        private void updateList()
+        {
+            //This invoke stuff is a bit 'complicated', don't pay attention to it. It's just.. this function is run in a separate thread
+            //You can't make changes from a different thread in the UI thread unless you invoke it.. not normally you would usually have
+            //to deal with.
+            this.Invoke(new MethodInvoker(delegate
+            {
+                dataGridView1.Rows.Clear();
+                //Iterate through all connected players and add their info to our list
+                foreach (Character ch in ConnectedPlayers)
+                {
+                    dataGridView1.Rows.Add(
+                        ch.Name, 
+                        ch.Connection.RemoteEndpoint.Address, 
+                        ch.Coordinates,
+                        ch.CurrentZone, 
+                        ch.Skin, 
+                        ch.Connection.AverageRoundtripTime);
+                }
+            }));
+        }
+
+        //Removes players from our list of connections
+        private void removeDisconnectedUser(NetConnection conn)
+        {
+            ConnectedPlayers.Remove(ConnectedPlayers.Where(f => f.Connection.RemoteUniqueIdentifier == conn.RemoteUniqueIdentifier).FirstOrDefault());
+            updateMovements();
+        }
+
+        //Simple function that allows us to log activity to our activity textbox, also known as "LET IT BE KNOWN!"
+        private void Write(string msg)
+        {
+            this.Invoke(new MethodInvoker(delegate
+            {
+                textBox1.AppendText(msg + Environment.NewLine);
+            }));
+        }
+    }
+
+    //Class used for simple functions to keep our main class tidy
+    public static class Misc
+    {
+
+    }
+
+    class gameObject
+    {
+        public string Name {get; set;}
+        public Point Coordinates { get; set;}
+    }
+
+    class Zone
+    {
+        public string Zones { get; set; }
+        public int MaxPlayers { get; set; }
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public string Skin { get; set; }
+        public Zones CurrentZone { get; set; }
+        public Point Coordinates { get; set; }
+        public NetConnection Connection { get; set; }
+        public Character(string name, Point coordinates, string charskin, NetConnection conn)
+        {
+            Skin = charskin;
+            Name = name;
+            Coordinates = coordinates;
+            Connection = conn;
+        }
+        public Character()
         {
 
         }
+    }
+
+    enum Zones
+    {
+        BirdIsland,
+        TwinCity,
+        Gotham
+    }
+
+    enum PacketTypes
+    {
+        LOGIN,
+        MOVE,
+        WORLDSTATE,
+        CHAT
     }
 }
